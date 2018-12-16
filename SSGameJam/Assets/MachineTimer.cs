f943c021595597e4d88e614e959f370bfa8a11a5
roland006 <roland006@rambler.ru>
2018-12-17 01:17:17 +0300

    Revert "Revert "Таймер на машинах (недоделан)""
    This reverts commit c46e54b258c31ce5364f86fd6d91562c4e0591d1.

---
+++ SSGameJam/Assets/MachineTimer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachineTimer : MonoBehaviour {
+
+    public Animator anim;
+    // Use this for initialization
+    void Start () {
+      
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+       if (transform.parent.GetComponent<machine>().timer == 0)
+       {
+            anim.SetInteger("State",4);
+            Debug.Log("1");
+        }
+       else if (transform.parent.GetComponent<machine>().timer == 1)
+       {
+            anim.SetInteger("State", 3);
+            Debug.Log("2");
+        }
+       else if (transform.parent.GetComponent<machine>().timer == 2)
+       {
+
+            anim.SetInteger("State", 2);
+            Debug.Log("3");
+        }
+       else if (transform.parent.GetComponent<machine>().timer == 3)
+       {
+            anim.SetInteger("State", 1);
+            Debug.Log("4");
+        }
+       else if (transform.parent.GetComponent<machine>().timer == 4)
+       {
+            anim.SetInteger("State", 0);
+            Debug.Log("5");
+        }
+    }
+}
