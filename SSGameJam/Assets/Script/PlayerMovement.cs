954da7385673d345d65fe5fe4dcf2a45e0cfa2c6
malmorsus <den21021996@gmail.com>
2018-12-17 15:13:16 +0300

    17.12 15:13

--- SSGameJam/Assets/Script/PlayerMovement.cs
+++ SSGameJam/Assets/Script/PlayerMovement.cs
@@ -8,7 +8,7 @@ public class PlayerMovement : MonoBehaviour {
     private Rigidbody2D rb;
     private Vector2 movement;
     public GameObject arms;
-
+    public Animator animator;
 
     // Use this for initialization
     void Start () {
@@ -20,14 +20,35 @@ public class PlayerMovement : MonoBehaviour {
         Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         movement = moveInput.normalized * speed;
 
+        //animator.SetFloat("Horizontal", moveInput.x);
+        //animator.SetFloat("Vertical", moveInput.y);
+
         if (moveInput.x == -1 && (moveInput.y >= 0 || moveInput.y <= 0))
+        {
             arms.transform.rotation = Quaternion.Euler(0, 0, 180);
+            animator.SetFloat("Horizontal", -1);
+            animator.SetFloat("Vertical", 0);
+        }
         if (moveInput.x == 1 && (moveInput.y >= 0 || moveInput.y <= 0))
+        {
             arms.transform.rotation = Quaternion.Euler(0, 0, 0);
+            animator.SetFloat("Horizontal", 1);
+            animator.SetFloat("Vertical", 0);
+        }
+
         if (moveInput.y == -1 && (moveInput.x >= 0 || moveInput.x <= 0))
+        {
             arms.transform.rotation = Quaternion.Euler(0, 0, -90);
+            animator.SetFloat("Horizontal", 0);
+            animator.SetFloat("Vertical", -1);
+        }
         if (moveInput.y == 1 && (moveInput.x >= 0 || moveInput.x <= 0))
+        {
             arms.transform.rotation = Quaternion.Euler(0, 0, 90);
+            animator.SetFloat("Horizontal", 0);
+            animator.SetFloat("Vertical", 1);
+        }
+
 
         if (moveInput.x == 1 && moveInput.y == 1)
             arms.transform.rotation = Quaternion.Euler(0, 0, 45);
