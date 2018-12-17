bf087aa94a3d580957bfb0b0eb24ac61fc57908f
Mikhail <roland006@rambler.ru>
2018-12-17 12:10:08 +0300

    Минимонитор с очками(не потестил)

--- SSGameJam/Assets/Script/QuestLog.cs
+++ SSGameJam/Assets/Script/QuestLog.cs
@@ -38,37 +38,9 @@ public class QuestLog : MonoBehaviour {
         for (int i = 0; i < 3; i++)
         {
             anim[i].SetInteger("Toy", log[i] - 100);
-
-            //if (log[i] == 100)
-            //{
-            //    anim[i].SetInteger("Toy", log[i]-100);
-            //    name[i] = "кукла";
-            //}
-            //else if (log[i] == 101)
-            //{
-            //    name[i] = "солдатик";
-            //}
-            //else if (log[i] == 102)
-            //{
-            //    name[i] = "машинка";
-            //}
-            //else if (log[i] == 103)
-            //{
-            //    name[i] = "вертолетик";
-            //}
-            //else if (log[i] == 104)
-            //{
-            //    name[i] = "кубики";
-            //}
-            //else
-            //{
-            //    name[i] = "робот";
-            //}
+            
         }
 
-        //GameObject TextBox = gameObject.transform.Find("TextBlock").gameObject;
-        //TextBox.GetComponent<TextMesh>().text = " " + name[0] + "; " + name[1] + "; " + name[2];
-        //Debug.Log(log[0] + " " + log[1] + " " + log[2]);
     }
 
     public void UpdateLog(int complete)
@@ -86,6 +58,7 @@ public class QuestLog : MonoBehaviour {
                 points = points + SuccessPoint;
                 i = 3;
                 Refresh(log[0], log[1], log[2]);
+                TextBox.GetComponent<TextMesh>().text = ""+points;
                 return;
             }
             count++;
