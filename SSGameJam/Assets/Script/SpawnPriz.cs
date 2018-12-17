009eb9ccd8db53e6b01db9fef161c02b7c0ad537
malmorsus <den21021996@gmail.com>
2018-12-17 21:17:38 +0300

    17.12 21:17

--- SSGameJam/Assets/Script/SpawnPriz.cs
+++ SSGameJam/Assets/Script/SpawnPriz.cs
@@ -7,7 +7,9 @@ public class SpawnPriz : MonoBehaviour {
     public GameObject spawnPoint;
     public Vector3 spawnPosition;
     public GameObject[] objectPrefab;
-
+    public Animator boxAnim;
+    public float t= -1;
+    public bool i;
 
     // Use this for initialization
     void Start()
@@ -18,11 +20,19 @@ public class SpawnPriz : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-
+        t -= Time.deltaTime;
+        if (t < 0 && i)
+        {
+            i = false;
+            boxAnim.SetBool("anim", false);
+            Instantiate(objectPrefab[Random.Range(0, objectPrefab.Length)], spawnPosition, Quaternion.identity);
+        }
     }
 
     public void SpawnP ()
     {
-        Instantiate(objectPrefab[Random.Range(0, objectPrefab.Length)], spawnPosition, Quaternion.identity);
+        t = 0.5f;
+        boxAnim.SetBool("anim", true);
+        i = true;
     }
 }
